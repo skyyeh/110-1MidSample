5893d0f7411a048c217fc6c68891703b746ecae6
冠登 廖 <t8599008@ntut.org.tw>
2021-10-28 18:46:32 +0800

    Methods finished

--- 110-1MidSample/Sample1Com.aspx.cs
+++ 110-1MidSample/Sample1Com.aspx.cs
@@ -10,5 +10,17 @@ namespace _110_1MidSample {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        // To convert a plain-text string into a md5 string
+        public string mt_2MD5(string s_Str) {
+            System.Security.Cryptography.MD5 cryptoMD5 = System.Security.Cryptography.MD5.Create();
+            byte[] ba_Bytes = System.Text.Encoding.UTF8.GetBytes(s_Str);
+            byte[] ba_Hash = cryptoMD5.ComputeHash(ba_Bytes);
+
+            string s_Md5 = BitConverter.ToString(ba_Hash)
+                .Replace("-", String.Empty)
+                .ToUpper();
+            return s_Md5;
+        }
     }
 }
