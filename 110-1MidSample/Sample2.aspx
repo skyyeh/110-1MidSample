0b94f7ff7f3d9d73f2bca282d798da12439f8277
于翔 林 <108111120@mail.aeust.edu.tw>
2021-11-11 20:45:19 +0800

    Sample 2

--- 110-1MidSample/Sample2.aspx
+++ 110-1MidSample/Sample2.aspx
@@ -10,6 +10,35 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h2>保單電訪單</h2>
+            <asp:Label runat="server" Text="保單編號" Font-Size="X-Large"></asp:Label>
+            <asp:Label ID="lb_Id" runat="server" Text=""></asp:Label><br />
+            <asp:Label runat="server" Text="聯絡方式" Font-Size="X-Large"></asp:Label>
+
+            <asp:DropDownList ID="ddl_Type" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_Type_SelectedIndexChanged">
+                <asp:ListItem>手機</asp:ListItem>
+                <asp:ListItem>市話</asp:ListItem>
+            </asp:DropDownList><br />
+
+            <asp:DropDownList ID="ddl_ZoneCode" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_ZoneCode_SelectedIndexChanged"></asp:DropDownList>
+
+            <asp:TextBox ID="tb_Number" runat="server"></asp:TextBox><br />
+            <asp:Label runat="server" Text="地址" Font-Size="X-Large"></asp:Label>
+            <asp:DropDownList ID="ddl_City" runat="server"></asp:DropDownList>
+            <asp:TextBox ID="tb_Add" runat="server"></asp:TextBox><br />
+
+            <asp:Label runat="server" Text="說明" Font-Size="X-Large"></asp:Label>
+            <asp:RadioButtonList ID="rb_Des" runat="server" AutoPostBack="True" OnSelectedIndexChanged="rb_Des_SelectedIndexChanged">
+                <asp:ListItem>否</asp:ListItem>
+                <asp:ListItem>是</asp:ListItem>
+            </asp:RadioButtonList>
+
+            <asp:TextBox ID="tb_Des" runat="server" Width="400px" Visible="False"></asp:TextBox> <br />
+
+            <asp:Button ID="btn_Submit" runat="server" Text="送出" Onclick ="btn_Submit_Click"/><br />
+
+            <asp:Label ID="lb_Msg" runat="server" Text=""></asp:Label>
+
         </div>
     </form>
 </body>
--- 110-1MidSample/Sample2.aspx.designer.cs
+++ 110-1MidSample/Sample2.aspx.designer.cs
@@ -7,10 +7,12 @@
 // </自動產生的>
 //------------------------------------------------------------------------------
 
-namespace _110_1MidSample {
+namespace _110_1MidSample
+{
 
 
-    public partial class Sample2 {
+    public partial class Sample2
+    {
 
         /// <summary>
         /// form1 控制項。
@@ -20,5 +22,95 @@ namespace _110_1MidSample {
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lb_Id 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lb_Id;
+
+        /// <summary>
+        /// ddl_Type 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_Type;
+
+        /// <summary>
+        /// ddl_ZoneCode 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_ZoneCode;
+
+        /// <summary>
+        /// tb_Number 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_Number;
+
+        /// <summary>
+        /// ddl_City 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_City;
+
+        /// <summary>
+        /// tb_Add 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_Add;
+
+        /// <summary>
+        /// rb_Des 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList rb_Des;
+
+        /// <summary>
+        /// tb_Des 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tb_Des;
+
+        /// <summary>
+        /// btn_Submit 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Submit;
+
+        /// <summary>
+        /// lb_Msg 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lb_Msg;
     }
 }
